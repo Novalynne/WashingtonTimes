8c64ef8c0ba4af35bdf0cad33586c9f784dad1fa
Novalynne <schirone.ma@gmail.com>
2025-04-17 04:22:09 +0200

    Added Right Colum

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+document.addEventListener("DOMContentLoaded", () => {
+    const audios = document.querySelectorAll("audio");
+
+    audios.forEach(audio => {
+        audio.addEventListener("play", () => {
+            audios.forEach(otherAudio => {
+                if (otherAudio !== audio) {
+                    otherAudio.pause();
+                    otherAudio.currentTime = 0;
+                }
+            });
+        });
+    });
+});
